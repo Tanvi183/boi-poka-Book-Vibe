b3f45747d410c033624d5777fc603b6212fb384a
tanvi183 <mneshat7@gmail.com>
2025-10-05 15:04:26 -0700

    Implement dynamic routes

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,37 +1,40 @@
 import React, { use } from 'react';
 import { FaStarHalfAlt } from "react-icons/fa";
+import { Link } from 'react-router';
 
 const Book = ({singleBook, bookPromise}) => {
     // const books = use(bookPromise);
     // console.log(books);
     console.log(singleBook);
-    const {bookName, author, image, rating, category, review, tags} = singleBook;
+    const {bookId, bookName, author, image, rating, category, review, tags} = singleBook;
 
     return (
-        <div className="card bg-base-100 w-96 shadow-sm border">
-            <figure className='p-3 bg-gray-100 w-4/5 mx-auto mt-4'>
-                <img
-                className='h-[166px]'
-                src={image}
-                alt="Shoes" />
-            </figure>
-            <div className="card-body">
-                <div className='flex justify-center gap-7'>
-                    {
-                        tags.map(tag => <button>{tag}</button>)
-                    }
-                </div>
-                <h2 className="card-title">
-                {bookName}
-                <div className="badge badge-secondary">{author}</div>
-                </h2>
-                <p className='line-clamp-3'>{review}</p>
-                <div className="card-actions justify-end">
-                <div className="badge badge-outline">{category}</div>
-                <div className="badge badge-outline">{rating} <FaStarHalfAlt /></div>
+        <Link to={`/bookDetails/${bookId}`}>
+            <div className="card bg-base-100 w-96 shadow-sm border">
+                <figure className='p-3 bg-gray-100 w-4/5 mx-auto mt-4'>
+                    <img
+                    className='h-[166px]'
+                    src={image}
+                    alt="Shoes" />
+                </figure>
+                <div className="card-body">
+                    <div className='flex justify-center gap-7'>
+                        {
+                            tags.map(tag => <button>{tag}</button>)
+                        }
+                    </div>
+                    <h2 className="card-title">
+                    {bookName}
+                    <div className="badge badge-secondary">{author}</div>
+                    </h2>
+                    <p className='line-clamp-3'>{review}</p>
+                    <div className="card-actions justify-end">
+                    <div className="badge badge-outline">{category}</div>
+                    <div className="badge badge-outline">{rating} <FaStarHalfAlt /></div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
